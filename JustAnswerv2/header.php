02e263f2902f02ee240ee5b63058842f21515477
sisyphos55 <benikayitet@yandex.com>
2024-02-03 20:16:07 +0300

    v3

---
+++ JustAnswerv2/header.php
@@ -0,0 +1,327 @@
+<html data-reactroot="">
+ <head>
+  <meta charset="utf-8"/>
+  <title>
+   Ask an Expert &amp; Get Answers to Your Questions - ASAP
+  </title>
+  <link href="my_downloaded_page_files/fav48.ico" rel="icon"/>
+  <link rel="stylesheet" href="https://tpr.5a3.myftpupload.com/wp-content/themes/JustAnswerTheme/style.css">
+  <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport"/>
+  <meta content="&amp;copy;2003-2020 JustAnswer LLC" name="copyright"/>
+  <meta content="Ask a question and get an answer to your question from a verified Expert on JustAnswer, the leading Expert question and answer website. Ask an Expert now!" name="description"/>
+  <meta content="Ask an Expert, Ask Questions Get Answers, Ask A Question Online, Ask Questions Online, Question And Answer, Ask A Question, Ask An Expert, Ask Questions, Get Answers, Ask Experts, JustAnswer, JustAnswer" id="meta_keywords" name="keywords"/>
+  <link as="script" href="my_downloaded_page_files/re-kv-page-home-page@1.11.2.js" rel="preload"/>
+  <link href="my_downloaded_page_files/ww2.justanswer.com" rel="dns-prefetch"/>
+  <link href="my_downloaded_page_files/ww2-secure.justanswer.com" rel="dns-prefetch"/>
+  <link href="my_downloaded_page_files/www.googleadservices.com" rel="dns-prefetch"/>
+  <link href="my_downloaded_page_files/www.google-analytics.com" rel="dns-prefetch"/>
+  <link href="my_downloaded_page_files/googleads.g.doubleclick.net" rel="dns-prefetch"/>
+  <link href="my_downloaded_page_files/connect.facebook.net" rel="dns-prefetch"/>
+  <meta content="onq/Jmry4CF5JcNYaSAF/wroRZDDuc3sl/jR76/BDYI=" name="verify-v1"/>
+  <meta content="1439148066368316" property="fb:app_id"/>
+  <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible"/>
+  <meta content="yes" name="apple-mobile-web-app-capable"/>
+  <meta content="summary_large_image" name="twitter:card"/>
+  <meta content="@justanswer" name="twitter:site"/>
+  <meta content="https://www.justanswer.com" property="twitter:url"/>
+  <meta content="Expert answers in minutes, 24/7" property="twitter:title"/>
+  <meta content="Talk to Experts in over 200 categories and get the information you need in no time." property="twitter:description"/>
+  <meta content="https://ww2-secure.justanswer.com/static/fe/re-kv-page-home-page/homepage-og.jpg" property="twitter:image"/>
+  <meta content="website" property="og:type"/>
+  <meta content="https://www.justanswer.com/" property="og:url"/>
+  <meta content="https://ww2-secure.justanswer.com/static/fe/re-kv-page-home-page/homepage-og.jpg" property="og:image"/>
+  <meta content="200" property="og:image:width"/>
+  <meta content="200" property="og:image:height"/>
+  <meta content="Expert answers in minutes, 24/7" property="og:title"/>
+  <meta content="Talk to Experts in over 200 categories and get the information you need in no time." property="og:description"/>
+  <meta content="JustAnswer" property="og:site_name"/>
+  <meta content="en_US" property="og:locale"/>
+  <meta content="index, follow" id="meta_robots" name="ROBOTS"/>
+  <script async="" src="my_downloaded_page_files/ad85e38d-8d4a-4a9c-80c7-289feb35db8a-latest.js" type="text/javascript">
+  </script>
+  <script async="" src="my_downloaded_page_files/A1345497-005d-4e0f-b02e-2eed4286e0701.js">
+  </script>
+  <script async="" src="my_downloaded_page_files/bat.js">
+  </script>
+  <script async="" src="my_downloaded_page_files/js?id=AW-1067616637&amp;l=dataLayer&amp;cx=c" type="text/javascript">
+  </script>
+  <script async="" src="my_downloaded_page_files/uwt.js">
+  </script>
+  <script async="" src="my_downloaded_page_files/roundtrip.js">
+  </script>
+  <script async="" src="my_downloaded_page_files/bat.js">
+  </script>
+  <script src="my_downloaded_page_files/ja-com.js">
+  </script>
+  <script async="" src="my_downloaded_page_files/ec.js" type="text/javascript">
+  </script>
+  <script async="" src="my_downloaded_page_files/analytics.js">
+  </script>
+  <script data-dtconfig="rid=RID_2418|rpid=1670143630|domain=justanswer.com|reportUrl=/rb_bf36546jsr|app=c5b11ceb0023d18f|cuc=v943g28l|mel=100000|featureHash=ICA2NVfghjqru|md=mdcc1=bJA.i.user.name|ssv=4|lastModification=1706120022170|vcv=2|tp=500,50,0,1|rdnt=0|uxrgce=1|bp=3|agentUri=/ruxitagentjs_ICA2NVfghjqru_10281231207105659.js" src="my_downloaded_page_files/ruxitagentjs_ICA2NVfghjqru_10281231207105659.js" type="text/javascript">
+  </script>
+  <link href="my_downloaded_page_files/" rel="canonical"/>
+  <link href="my_downloaded_page_files/fav48.ico" rel="shortcut icon" type="image/x-icon"/>
+  <link href="my_downloaded_page_files/ja-boxlogo.png" rel="apple-touch-icon"/>
+
+  
+
+ 
+  <script async="" id="ja-ui-trustpilot-script" src="my_downloaded_page_files/tp.widget.bootstrap.min.js" type="text/javascript">
+  </script>
+  <script data-cfasync="false" data-sync="off" src="my_downloaded_page_files/airgap.js">
+  </script>
+  <script src="my_downloaded_page_files/js?id=G-YVR58N9NM0" type="text/javascript">
+  </script>
+  <script crossorigin="anonymous" src="my_downloaded_page_files/revizely@released.js" type="text/javascript">
+  </script>
+  <script crossorigin="anonymous" src="my_downloaded_page_files/client-side-internal-tracking@released.js" type="text/javascript">
+  </script>
+  <script crossorigin="anonymous" src="my_downloaded_page_files/remarketing-pixels-us@released.js" type="text/javascript">
+  </script>
+  <script crossorigin="anonymous" src="my_downloaded_page_files/broken-resources-on-seo-pages@released.js" type="text/javascript">
+  </script>
+  <script crossorigin="anonymous" src="my_downloaded_page_files/component-model-tracking@released.js" type="text/javascript">
+  </script>
+  <script crossorigin="anonymous" src="my_downloaded_page_files/on-home-page-tracking-us@released.js" type="text/javascript">
+  </script>
+  <script crossorigin="anonymous" src="my_downloaded_page_files/traffic-source@released.js" type="text/javascript">
+  </script>
+  <script crossorigin="anonymous" src="my_downloaded_page_files/chat-tracking@released.js" type="text/javascript">
+  </script>
+  <script crossorigin="anonymous" src="my_downloaded_page_files/remarketing-pixels-bing@released.js" type="text/javascript">
+  </script>
+  <script crossorigin="anonymous" src="my_downloaded_page_files/web-vitals-tracking@released.js" type="text/javascript">
+  </script>
+  <script crossorigin="anonymous" src="my_downloaded_page_files/affiliate-traffic-source@released.js" type="text/javascript">
+  </script>
+  <script crossorigin="anonymous" src="my_downloaded_page_files/remarketing-pixels-impact@released.js" type="text/javascript">
+  </script>
+  <script crossorigin="anonymous" src="my_downloaded_page_files/cookie-size-exceeded-tracking@released.js" type="text/javascript">
+  </script>
+  <script crossorigin="anonymous" src="my_downloaded_page_files/tatari-pixels@released.js" type="text/javascript">
+  </script>
+  <script crossorigin="anonymous" src="my_downloaded_page_files/pandora-pixels@released.js" type="text/javascript">
+  </script>
+  <meta content="AymqwRC7u88Y4JPvfIF2F37QKylC04248hLCdJAsh8xgOfe/dVJPV3XS3wLFca1ZMVOtnBfVjaCMTVudWM//5g4AAAB7eyJvcmlnaW4iOiJodHRwczovL3d3dy5nb29nbGV0YWdtYW5hZ2VyLmNvbTo0NDMiLCJmZWF0dXJlIjoiUHJpdmFjeVNhbmRib3hBZHNBUElzIiwiZXhwaXJ5IjoxNjk1MTY3OTk5LCJpc1RoaXJkUGFydHkiOnRydWV9" http-equiv="origin-trial"/>
+  <script src="my_downloaded_page_files/9327b182-5de5-4429-85b9-c68c2e48d3bf.js" type="text/javascript">
+  </script>
+ </head>
+ <body>
+  <img border="0" height="1" src="my_downloaded_page_files/?ad_log=referer&amp;action=lead&amp;pixid=94966476-557e-49e0-9142-b968db28c3ae" width="1"/>
+  <meta content="A41wt2Lsq30A9Ox/WehogvJckPI4aY9RoSxhb8FMtVnqaUle1AtI6Yf7Wk+7+Wm0AfDDOkMX+Wn6wnDpBWYgWwYAAAB8eyJvcmlnaW4iOiJodHRwczovL2Fkcm9sbC5jb206NDQzIiwiZmVhdHVyZSI6IkludGVyZXN0Q29ob3J0QVBJIiwiZXhwaXJ5IjoxNjI2MjIwNzk5LCJpc1N1YmRvbWFpbiI6dHJ1ZSwiaXNUaGlyZFBhcnR5Ijp0cnVlfQ==" http-equiv="origin-trial"/>
+  <div class="re-kv-page-home-page">
+   <div class="App__container">
+    <div class="DonationBanner__container">
+     <div class="DonationBanner__content">
+      <div class="DonationBanner__flag">
+      </div>
+      <div class="DonationBanner__content--medium">
+       Join JustAnswer in supporting our friends and colleagues in Ukraine. Help Ukraine stand strong.
+       <div class="DonationBanner__btn-donate">
+        Donate now
+       </div>
+      </div>
+      <div class="DonationBanner__content--small">
+       Join JustAnswer in supporting our friends and colleagues in Ukraine.
+       <div class="DonationBanner__btn-donate">
+        Donate now
+       </div>
+      </div>
+     </div>
+     <div class="DonationBanner__btn-close">
+      <svg class="sc-jMOgbH ldsAvz" fill="none" height="12" viewbox="0 0 12 12" width="12" xmlns="http://www.w3.org/2000/svg">
+       <path d="M7.14948 6.08842C7.13784 6.07681 7.1286 6.06302 7.1223 6.04783C7.116 6.03265 7.11276 6.01637 7.11276 5.99993C7.11276 5.98349 7.116 5.96721 7.1223 5.95202C7.1286 5.93683 7.13784 5.92304 7.14948 5.91143L11.7806 1.28062C11.9212 1.13979 12.0001 0.948872 12 0.749857C11.9999 0.550842 11.9207 0.360034 11.7799 0.219408C11.639 0.0787831 11.4481 -0.000140457 11.2491 1.87653e-07C11.0501 0.000140832 10.8593 0.0793341 10.7187 0.220158L6.08807 4.84897C6.07646 4.86061 6.06266 4.86985 6.04748 4.87615C6.03229 4.88245 6.01601 4.8857 5.99957 4.8857C5.98313 4.8857 5.96685 4.88245 5.95167 4.87615C5.93648 4.86985 5.92269 4.86061 5.91108 4.84897L1.28046 0.220158C1.21083 0.150495 1.12816 0.0952284 1.03718 0.0575143C0.946193 0.0198001 0.848671 0.000376929 0.750179 0.000353716C0.551266 0.000306834 0.360481 0.079283 0.219796 0.219908C0.0791097 0.360534 4.69011e-05 0.551289 2.086e-08 0.75021C-4.68594e-05 0.949132 0.078926 1.13992 0.219546 1.28062L4.84967 5.91143C4.86131 5.92304 4.87054 5.93683 4.87684 5.95202C4.88314 5.96721 4.88639 5.98349 4.88639 5.99993C4.88639 6.01637 4.88314 6.03265 4.87684 6.04783C4.87054 6.06302 4.86131 6.07681 4.84967 6.08842L0.219546 10.7197C0.149918 10.7894 0.0946926 10.8721 0.0570229 10.9631C0.0193532 11.0541 -2.31919e-05 11.1516 2.086e-08 11.2501C4.69011e-05 11.4491 0.0791097 11.6398 0.219796 11.7804C0.289456 11.8501 0.372149 11.9053 0.463152 11.943C0.554155 11.9806 0.651687 12 0.750179 12C0.949092 12 1.13984 11.9209 1.28046 11.7802L5.91108 7.14888C5.92269 7.13724 5.93648 7.12801 5.95167 7.1217C5.96685 7.1154 5.98313 7.11216 5.99957 7.11216C6.01601 7.11216 6.03229 7.1154 6.04748 7.1217C6.06266 7.12801 6.07646 7.13724 6.08807 7.14888L10.7187 11.7802C10.8593 11.9209 11.0501 12 11.249 12C11.4479 12 11.6387 11.9211 11.7794 11.7804C11.92 11.6398 11.9991 11.4491 11.9991 11.2501C11.9992 11.0512 11.9202 10.8604 11.7796 10.7197L7.14948 6.08842Z" fill="#EBEBEB">
+       </path>
+      </svg>
+     </div>
+    </div>
+    <!-- -->
+    <!-- -->
+    <header class="Hero__container">
+     <div class="Hero__background">
+      <div class="Hero__content">
+       <div class="Navigation__container">
+        <nav class="TopNavigation__container TopNavigation__container--fixed">
+         <div class="TopNavigation__logo-container">
+          <div>
+           <a class="css-1x48qdj" href="#" title="JustAnswer.com">
+           </a>
+          </div>
+         </div>
+         <div style="opacity:0">
+          <svg class="sc-fznMnq biVOqy" fill="none" height="12" viewbox="0 0 12 12" width="12" xmlns="http://www.w3.org/2000/svg">
+           <g clip-path="url(#clip0)">
+            <path clip-rule="evenodd" d="M1.2 4.6C0.705573 3.94076 1.17595 3 2 3H10C10.824 3 11.2944 3.94076 10.8 4.6L6.8 9.93333C6.4 10.4667 5.6 10.4667 5.2 9.93333L1.2 4.6Z" fill="#333333" fill-rule="evenodd">
+            </path>
+           </g>
+           <defs>
+            <clippath id="clip0">
+             <path d="M0 0H12V12H0V0Z" fill="white">
+             </path>
+            </clippath>
+           </defs>
+          </svg>
+         </div>
+         <div class="TopNavigation__spacer">
+         </div>
+         <div class="TopNavigation__display-none">
+          <ul class="TopNavigation__list">
+           <li class="TopNavigation__list__item TopNavigation__list__item--main" tabindex="0">
+            <a class="TopNavigation__list__link" href="#">
+             About us
+             <span class="TopNavigation__list__arrow">
+              <svg class="sc-fzoant hXtGXC" fill="none" height="12" viewbox="0 0 12 12" width="12" xmlns="http://www.w3.org/2000/svg">
+               <g clip-path="url(#clip0)">
+                <path clip-rule="evenodd" d="M1.2 4.6C0.705573 3.94076 1.17595 3 2 3H10C10.824 3 11.2944 3.94076 10.8 4.6L6.8 9.93333C6.4 10.4667 5.6 10.4667 5.2 9.93333L1.2 4.6Z" fill="#0E3C48" fill-rule="evenodd">
+                </path>
+               </g>
+               <defs>
+                <clippath id="clip0">
+                 <path d="M0 0H12V12H0V0Z" fill="white">
+                 </path>
+                </clippath>
+               </defs>
+              </svg>
+             </span>
+            </a>
+           </li>
+           <li class="TopNavigation__list__item">
+            <a class="TopNavigation__list__link" href="https://www.justanswer.com/pricing">
+             Pricing
+            </a>
+           </li>
+           <li class="TopNavigation__list__item">
+            <a class="TopNavigation__list__link" href="https://era.justanswer.com/?utm_source=JustAnswer&amp;utm_medium=referral&amp;utm_campaign=home-page-header">
+             For Experts
+            </a>
+           </li>
+           <li class="TopNavigation__list__item">
+            <a class="TopNavigation__list__link" href="https://www.justanswer.com/info/partner-program">
+             For Partners
+            </a>
+           </li>
+           <li class="TopNavigation__list__item">
+            <a class="TopNavigation__list__link" href="https://www.justanswer.com/help">
+             Help
+            </a>
+           </li>
+          </ul>
+         </div>
+         <a class="TopNavigation__login" href="https://www.justanswer.com/login.aspx">
+          Log in
+         </a>
+         <div class="Burger__container">
+          <div class="Burger__container__line">
+          </div>
+          <div class="Burger__container__line">
+          </div>
+          <div class="Burger__container__line">
+          </div>
+         </div>
+        </nav>
+       </div>
+       <div class="Header__container">
+        <div class="Header__title-container Header__title-container--long">
+         <h1 class="Header__title">
+          Revolutionizing professional services
+         </h1>
+         <h3 class="Header__subtitle">
+          24/7 access to help from thousands of Experts, where and when you need it.
+         </h3>
+        </div>
+        <div class="Header__title-container Header__title-container--short">
+         <h1 class="Header__title">
+          Revolutionizing professional services
+         </h1>
+         <h3 class="Header__subtitle">
+          24/7 access to help from thousands of Experts, where and when you need it.
+         </h3>
+        </div>
+        <div class="QuestionBoxForm__background false">
+        </div>
+        <form action="https://my-secure.justanswer.com/myhome" class="QuestionBoxForm__container false" method="post">
+         <div class="QuestionBoxForm__header">
+          <p class="QuestionBoxForm__header__title">
+           Ask a question
+          </p>
+          <div class="QuestionBoxForm__header__close-button">
+           <svg class="sc-jSnUir faEPjD" fill="none" height="24" viewbox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+            <g>
+             <path clip-rule="evenodd" d="M23.7071 1.70711C24.0976 1.31658 24.0976 0.683417 23.7071 0.292893C23.3166 -0.0976311 22.6834 -0.0976311 22.2929 0.292893L12 10.5858L1.70711 0.292893C1.31658 -0.0976311 0.683417 -0.0976311 0.292893 0.292893C-0.0976311 0.683417 -0.0976311 1.31658 0.292893 1.70711L10.5858 12L0.292893 22.2929C-0.0976311 22.6834 -0.0976311 23.3166 0.292893 23.7071C0.683417 24.0976 1.31658 24.0976 1.70711 23.7071L12 13.4142L22.2929 23.7071C22.6834 24.0976 23.3166 24.0976 23.7071 23.7071C24.0976 23.3166 24.0976 22.6834 23.7071 22.2929L13.4142 12L23.7071 1.70711Z" fill="#333333" fill-rule="evenodd">
+             </path>
+            </g>
+            <defs>
+             <clippath id="clip0">
+              <rect fill="white" height="24" width="24">
+              </rect>
+             </clippath>
+            </defs>
+           </svg>
+          </div>
+         </div>
+         <div class="QuestionBoxForm__input-field">
+          <textarea autocomplete="off" class="QuestionBoxForm__textbox" maxlength="5000" name="question"></textarea>
+          <div class="QuestionBoxForm__placeholder QuestionBoxForm__placeholder--main">
+           <div class="QuestionBoxPlaceholder__placeholder-container false">
+            <p>
+             Ask
+             <!-- -->
+            </p>
+            <p class="QuestionBoxPlaceholder__dynamic QuestionBoxPlaceholder__dynamic--erasing">
+             a contractor...
+            </p>
+           </div>
+          </div>
+          <div class="QuestionBoxForm__placeholder QuestionBoxForm__placeholder--additional">
+           <div class="QuestionBoxPlaceholder__placeholder-container false">
+            <p>
+             Type your question here...
+             <!-- -->
+            </p>
+            <p class="QuestionBoxPlaceholder__dynamic QuestionBoxPlaceholder__dynamic--erasing">
+            </p>
+           </div>
+          </div>
+         </div>
+         <div class="QuestionBoxForm__submit-container false">
+          <div class="QuestionBoxForm__validation-error QuestionBoxForm__validation-error--hidden">
+           Enter your question to continue...
+          </div>
+          <button class="QuestionBoxForm__submit-button" type="submit">
+           <p class="QuestionBoxForm__submit-button__text">
+            Start now
+           </p>
+           <p class="QuestionBoxForm__submit-button__text--expanded">
+            Continue
+           </p>
+          </button>
+         </div>
+        </form>
+        <div class="QuestionBoxMinimized__container false">
+         <div class="QuestionBoxMinimized__placeholder">
+          <div class="QuestionBoxPlaceholder__placeholder-container false">
+           <p>
+            Ask
+            <!-- -->
+           </p>
+           <p class="QuestionBoxPlaceholder__dynamic QuestionBoxPlaceholder__dynamic--erasing">
+            a contractor...
+           </p>
+          </div>
+         </div>
+         <p class="QuestionBoxMinimized__question-preview">
+         </p>
+         <div class="QuestionBoxMinimized__submit-container">
+          <button class="QuestionBoxMinimized__submit-button" type="button">
+           <p>
+            Start now
+           </p>
+          </button>
+         </div>
+        </div>
+       </div>
+       <span style="font-size:0">
+       </span>
+      </div>
+     </div>
+    </header>
